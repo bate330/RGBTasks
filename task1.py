136a04e1e062b8511c18390771a674a530fd3211
bate330 <bateu.email@yahoo.fr>
2021-12-09 10:32:30 +0100

    'Comments'

--- task1.py
+++ task1.py
@@ -5,27 +5,27 @@ class Main():
         self.minValue = minValue
         self.maxValue = maxValue
 
-    def __IterateValue(self, iterateValue):
+    def __IterateValue(self, iterateValue): #iterate number from arg
         iterateStr = str(iterateValue)
         isPassed = 0
-        for element in range(1, len(iterateStr)):
-            if iterateStr[element] == iterateStr[element-1]:
+        for element in range(1, len(iterateStr)):   #iterate all digit from iterate value
+            if iterateStr[element] == iterateStr[element-1]:    #check if is it pair
                 isPassed = 1
             else:
                 pass
-            if iterateStr[element-1] > iterateStr[element]:
+            if iterateStr[element-1] > iterateStr[element]: #check if raise
                 isPassed = 0
                 break
         return isPassed
 
-    def __CheckNumbers(self):
+    def __CheckNumbers(self):   #check numbers from range and put into iterate
         results = 0
         for i in range(self.minValue, self.maxValue):
             results += self.__IterateValue(i)
         return results
 
-    def GetResult(self):
-        return self.__CheckNumbers()
+    def GetResult(self):    #get final result
+        return self.__CheckNumbers()    #get number of correct solutions only
 
 minValue = 265275
 maxValue = 781584
--- task2.py
+++ task2.py
@@ -6,17 +6,17 @@ class Main():
         self.params = params
         self.paramsWithCondition = paramsWithCondition
 
-    def __OpenFile(self, filename):
+    def __OpenFile(self, filename): #read-only file
         file = open(filename,"r")
         return file.readlines()
 
-    def __Refactor(self):
+    def __Refactor(self):   #remove special characters
         content = self.__OpenFile(self.filename)
-        content.append('\n')
+        content.append('\n')    #eof
         refactoredContent = []
         oneDoc = ''
-        for i in content:
-            if i == '\n':
+        for i in content:   #for each line
+            if i == '\n':   #if empty line
                 refactoredContent.append(oneDoc)
                 oneDoc = ''
                 continue
@@ -25,34 +25,37 @@ class Main():
                 oneDoc += i
         return self.__GetDictionary(refactoredContent)
 
-    def __ConvertToDict(self, lst):
+    def __ConvertToDict(self, lst): #convert list to dictionary
         res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
         return res_dct
 
-    def __GetDictionary(self, content):
+    def __GetDictionary(self, content): #get keys and values to create ditctionary
         pairsToDict = []
-        for i in content:
+        for i in content:   #get pairs(key:value) from file by space
             pairsToDict.append(i.split())
         allElementsOfDict = []
         dictionary = []
-        for attrib in pairsToDict:
+        for attrib in pairsToDict:  #for each doc
             allElementsOfDict.clear()
-            for param in attrib:
+            for param in attrib:    #for each pair in one doc
                 allElementsOfDict.append(param.split(':')[0])   #get key
                 allElementsOfDict.append(param.split(':')[1])   #get value
-            dictionary.append(self.__ConvertToDict(allElementsOfDict))
+            dictionary.append(self.__ConvertToDict(allElementsOfDict))  #create dictionaries
         return dictionary
 
-    def __GetCorrectDocs(self, dictionary):
+    def __GetCorrectDocs(self, dictionary): #get a number with correct documents only
         correctDocs = 0
-        for doc in dictionary:
-           if set(doc) == set(self.params) or set(doc) == set(self.paramsWithCondition):
+        for doc in dictionary:  #iterate through all documents in file
+           if set(doc) == set(self.params) or set(doc) == set(self.paramsWithCondition):    #if keys correct
                correctDocs += 1
         return correctDocs
 
-    def GetResult(self):
-        content = self.__Refactor()
-        correctDocs = self.__GetCorrectDocs(content)
+    def GetResult(self):    #get final result
+        try:
+            content = self.__Refactor() #refactor content from file
+            correctDocs = self.__GetCorrectDocs(content)    #count correct docs
+        except:
+            correctDocs = 'No file'
         return correctDocs
 
 
